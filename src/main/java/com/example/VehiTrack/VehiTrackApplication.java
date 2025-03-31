ed22a615ed2727a505f386ba81fd8b25b9eb4dbf
Alberto <albertojunior848423803@gmail.com>
2025-03-31 17:13:25 +0300

    first commit

---
+++ src/main/java/com/example/VehiTrack/VehiTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.VehiTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VehiTrack/VehiTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VehiTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
