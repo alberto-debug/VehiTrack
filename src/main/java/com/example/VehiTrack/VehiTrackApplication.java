8611e1e0d52cb501475660bd897526d57c3eee60
Alberto <albertojunior848423803@gmail.com>
2025-03-31 17:28:16 +0300

    changes

--- src/main/java/com/example/VehiTrack/VehiTrackApplication.java
+++ src/main/java/com/example/VehiTrack/VehiTrackApplication.java
@@ -2,12 +2,24 @@ package com.example.VehiTrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/home")
 @SpringBootApplication
 public class VehiTrackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehiTrackApplication.class, args);
+
+	}
+
+	@GetMapping("/test")
+	public String Home() {
+		return "Hello There";
+		System.out.Print("Running at: http://localhost:8080/home/test");
 	}
 
 }
