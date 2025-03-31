74964379ca9903d8046750d9986daac6c4a6e25c
Alberto <albertojunior848423803@gmail.com>
2025-03-31 18:50:30 +0300

    changes

--- src/main/java/com/example/VehiTrack/service/vehiService.java
+++ src/main/java/com/example/VehiTrack/service/vehiService.java
@@ -1,4 +1,18 @@
 package com.example.VehiTrack.service;
 
+import com.example.VehiTrack.model.Vehicle;
+import com.example.VehiTrack.repository.vehiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class vehiService {
+
+    @Autowired
+    private vehiRepository vehirepository;
+
+    public Vehicle registerVehicles(Vehicle vehicle){
+
+        return vehirepository.save(vehicle);
+    }
 }
