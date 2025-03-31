86199cc76ec7e92e3cd8d39f51df055ee422ebc3
Alberto <albertojunior848423803@gmail.com>
2025-03-31 19:01:16 +0300

    changes

---
+++ src/main/java/com/example/VehiTrack/model/VehicleDTO.java
@@ -0,0 +1,9 @@
+package com.example.VehiTrack.model;
+
+public class VehicleDTO {
+
+    private String marca;
+    private String modelo;
+    private String chassi;
+    private boolean licenciado;
+}
