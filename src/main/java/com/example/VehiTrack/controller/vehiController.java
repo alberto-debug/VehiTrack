f6752cb0b09b38bd71266e80e7fa0aca59c0bd89
Alberto <albertojunior848423803@gmail.com>
2025-03-31 19:17:00 +0300

    changes

--- src/main/java/com/example/VehiTrack/controller/vehiController.java
+++ src/main/java/com/example/VehiTrack/controller/vehiController.java
@@ -1,4 +1,25 @@
 package com.example.VehiTrack.controller;
 
+import com.example.VehiTrack.model.Vehicle;
+import com.example.VehiTrack.service.vehiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vehicles")
 public class vehiController {
+
+    @Autowired
+    private vehiService vehiService;
+
+    @PostMapping
+    public ResponseEntity<Vehicle> registerVehicle(@RequestBody Vehicle vehicle){
+        Vehicle registeredVehicle = vehiService.registerVehicles(vehicle);
+
+
+    }
 }
