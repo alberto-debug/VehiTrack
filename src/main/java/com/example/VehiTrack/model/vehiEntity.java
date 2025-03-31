cec0c157035439cbfd3acd0cefc79a3b23abc10b
Alberto <albertojunior848423803@gmail.com>
2025-03-31 17:43:08 +0300

    changes

--- src/main/java/com/example/VehiTrack/model/vehiEntity.java
+++ src/main/java/com/example/VehiTrack/model/vehiEntity.java
@@ -1,5 +1,21 @@
 package com.example.VehiTrack.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "vehicle")
 public class vehiEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String proprietario;
+
+    @Column(nullable = false , unique = true)
+    private int cpf;
+
+    @Column(nullable = false , unique = true)
+    private String placa;
 
 }
