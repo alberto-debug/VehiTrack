99677c1a57eea7eeed9bf55ef356920ee1589499
Alberto <albertojunior848423803@gmail.com>
2025-04-05 01:54:44 +0300

    changes

--- src/main/java/com/example/VehiTrack/repository/VehiRepository.java
+++ src/main/java/com/example/VehiTrack/repository/VehiRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VehiRepository extends JpaRepository<Vehicle, Long> {
-  Optional findById(String id);
+  Optional findById(Long id);
 
 }
