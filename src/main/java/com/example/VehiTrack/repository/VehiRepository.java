fcaa6bd3e0b431d59103f07e4f5096e7993d1710
Alberto <albertojunior848423803@gmail.com>
2025-04-05 01:16:06 +0300

    changes

--- src/main/java/com/example/VehiTrack/repository/VehiRepository.java
+++ src/main/java/com/example/VehiTrack/repository/VehiRepository.java
@@ -1,10 +1,14 @@
 package com.example.VehiTrack.repository;
 
 import com.example.VehiTrack.model.Vehicle;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VehiRepository {
+public interface VehiRepository extends JpaRepository<Vehicle, Long> {
+  Optional findByEmail(String email);
 
 }
