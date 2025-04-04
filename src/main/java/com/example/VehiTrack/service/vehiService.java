0b54f2e83ff7639e75c2e229be707a89604f3f9a
Alberto <albertojunior848423803@gmail.com>
2025-04-05 00:49:16 +0300

    changes

--- src/main/java/com/example/VehiTrack/service/vehiService.java
+++ src/main/java/com/example/VehiTrack/service/vehiService.java
@@ -11,27 +11,4 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class vehiService {
 
-    @Autowired
-    private VehiRepository vehirepository;
-
-    public Vehicle registerVehicles(Vehicle vehicle) {
-        try {
-            String apiUrls = "https://my.api.mockaroo.com/veiculos?key=55ad1cd0&placa=" + vehicle.getPlaca();
-            RestTemplate restTemplate = new RestTemplate();
-            VehicleDetails vehicleDetails = restTemplate.getForObject(apiUrls, VehicleDetails.class);
-
-            if (vehicleDetails != null) {
-                vehicle.setMarca(vehicleDetails.getMarca());
-                vehicle.setModelo(vehicleDetails.getModelo());
-                vehicle.setChassi(vehicleDetails.getChassi());
-                vehicle.setLicenciado(vehicleDetails.getLicenciado());
-            } else {
-                throw new RuntimeException("Failed to fetch vehicle details from external API");
-            }
-        } catch (HttpClientErrorException e) {
-            throw new RuntimeException("Failed to fetch vehicle details from external API: " + e.getMessage());
-        }
-
-        return vehirepository.save(vehicle);
-    }
 }
