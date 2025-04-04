c9c36fc51a74f21aacacfc65db9b856e6f68717c
Alberto <albertojunior848423803@gmail.com>
2025-04-05 00:47:14 +0300

    changes

--- src/main/java/com/example/VehiTrack/model/VehicleDetails.java
+++ src/main/java/com/example/VehiTrack/model/VehicleDetails.java
@@ -3,28 +3,6 @@ package com.example.VehiTrack.model;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
 public class VehicleDetails {
 
-    private String marca;
-    private String modelo;
-    private String chassi;
-    private boolean licenciado;
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public String getChassi() {
-        return chassi;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public boolean getLicenciado() {
-        return licenciado;
-    }
 }
