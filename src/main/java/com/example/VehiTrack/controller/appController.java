be0b667e8e132869635f25b553603d6a468513e2
Alberto <albertojunior848423803@gmail.com>
2025-04-05 00:58:57 +0300

    changes

--- src/main/java/com/example/VehiTrack/controller/appController.java
+++ src/main/java/com/example/VehiTrack/controller/appController.java
@@ -1,4 +1,14 @@
 package com.example.VehiTrack.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class appController {
+
+  @PostMapping("/")
+  public String hello() {
+    return ("HELLO, KINDLY NOTE THAT YOUR APPLICATION IS LIVE");
+  }
+
 }
