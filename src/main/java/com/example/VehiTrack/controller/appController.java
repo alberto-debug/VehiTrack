8bbae8ddc38cb8a2e1c33b3a1517362184c18d31
Alberto <albertojunior848423803@gmail.com>
2025-04-05 01:07:57 +0300

    changes

--- src/main/java/com/example/VehiTrack/controller/appController.java
+++ src/main/java/com/example/VehiTrack/controller/appController.java
@@ -1,12 +1,13 @@
 package com.example.VehiTrack.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class appController {
 
-  @PostMapping("/")
+  @GetMapping("/")
   public String hello() {
     return ("HELLO, KINDLY NOTE THAT YOUR APPLICATION IS LIVE");
   }
--- src/main/java/com/example/VehiTrack/model/Vehicle.java
+++ src/main/java/com/example/VehiTrack/model/Vehicle.java
@@ -5,6 +5,25 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "vehicles")
 public class Vehicle {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String owner;
+
+    private String identification;
+
+    private String placa;
+
+    private String brand;
+
+    private String model;
+
+    private String chassi;
+
+    private String licensed;
 }
